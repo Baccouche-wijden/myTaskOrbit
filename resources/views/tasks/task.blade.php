d0a9b8271bacd4b68d48ff97f88eccf0392a0076
unknown <baccouche.wijden@gmail.com>
2025-02-26 14:01:01 +0100

    task blade check

--- resources/views/tasks/task.blade.php
+++ resources/views/tasks/task.blade.php
@@ -45,8 +45,6 @@
                 </div>
 
 
-
-
                 <div class="flex items-center justify-end mt-4">
                     <button class="ms-4 inline-flex items-center px-4 py-2  border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" style="background-color: #ff5f00">
                         {{ __('Add Task') }}
